9defc17ca647d752a4581b7cd1de8a0608aea7d1
KozlitinaValeria <kozlitinavaleriia@gmail.com>
2020-01-17 19:43:19 +0200

    Task 1 complited

---
+++ The crimes/file/task1.py
@@ -0,0 +1,35 @@
+indexes_to_int = [0,2,3,4]
+indexes_to_bool = [7]
+
+with open ('../Crimes.csv') as file:
+    header = file.readline().rstrip().split(',')
+    print(header)
+    for line in file:
+        crime_list = line.rstrip().split(',')
+        for index in indexes_to_int:
+            if crime_list[index] != '-':
+                crime_list[index] == int(crime_list[index])
+
+        for index in indexes_to_bool:
+            crime_list[index] == bool(crime_list[index])
+            print(crime_list)
+
+        for index in indexes_to_bool:
+            if crime_list[index] == "True":
+                crime_list[index] = True
+            else:
+                crime_list[index] = False
+
+        name = crime_list.pop(INDEX_OF_NAME)
+
+        crime_dict = {}
+        for i in range(len(header)):
+            crime_dict[header[i]] = crime_list[i]
+
+        if name in crimes:
+            crimes[name].append(crime_dict)
+        else:
+            crimes[name] = [crime_dict]
+
+
+
